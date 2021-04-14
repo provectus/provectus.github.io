2613337ebf80b6f9fc9d0a10cb97457b27dcfd44
Marat Galiev <kazanlug@gmail.com>
2021-04-13 21:53:37 -0400

    Initial

---
+++ js/projects.js
@@ -0,0 +1,19 @@
+oss_projects = {};
+oss_projects['kafka-ui'] = {
+  blog_post: '',
+  featured: true,
+  position: 1,
+  background: 'github.png'
+};
+oss_projects['swiss-army-kube'] = {
+  blog_post: '',
+  featured: true,
+  position: 2,
+  background: 'github.png'
+};
+oss_projects['sak-kubeflow'] = {
+  blog_post: '',
+  featured: true,
+  position: 3,
+  background: 'github.png'
+};
